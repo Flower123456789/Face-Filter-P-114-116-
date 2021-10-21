a1f305c8f5e250bc894162636c3b936455eb8484
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2021-10-21 16:12:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
-function preload(){
+noseX=0;
+noseY=0;
 
+
+function preload(){
+    mustache = loadImage('https://i.postimg.cc/yYGt2sBW/musache.png');
+sungalsses = loadImage('https://i.postimg.cc/sD2CtThH/sungalsses.png');
 }
 
 
@@ -19,6 +24,8 @@ function setup(){
 function draw(){
  
     image(video, 0,0, 300, 300);
+    image(mustache, noseX , noseY+10, 30 ,30);
+    image(sungalsses, noseX-30, noseY-30, 95, 40);
 }
 
 
@@ -37,6 +44,8 @@ function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-10;
         console.log("nose x =" + results[0].pose.nose.x);
         console.log("nose y =" + results[0].pose.nose.y);
 }
